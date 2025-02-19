f28bf449a8ff18c9ff351f51be9a518666bade52
Estherlvn <estherlvn@gmail.com>
2025-02-19 16:03:24 +0100

    Configuration AppCustomAuthenticator +création vues home pour chaque role

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -2,49 +2,65 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @see https://symfony.com/doc/current/security/custom_authenticator.html
  */
 class AppCustomAuthenticator extends AbstractAuthenticator
 {
+    private UrlGeneratorInterface $urlGenerator;
+    
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+    
     /**
-     * Called on every request to decide if this authenticator should be
-     * used for the request. Returning `false` will cause this authenticator
-     * to be skipped.
+     * Détermine si l'authentificateur doit s'activer pour la requête courante.
+     * Ici, il s'active uniquement pour les requêtes POST sur la route "app_login".
      */
     public function supports(Request $request): ?bool
     {
-        // return $request->headers->has('X-AUTH-TOKEN');
+        return $request->attributes->get('_route') === 'app_login' && $request->isMethod('POST');
     }
-
+    
+    /**
+     * Extrait les informations d'identification de la requête et retourne un Passport.
+     */
     public function authenticate(Request $request): Passport
     {
-        // $apiToken = $request->headers->get('X-AUTH-TOKEN');
-        // if (null === $apiToken) {
-        // The token header was empty, authentication fails with HTTP Status
-        // Code 401 "Unauthorized"
-        // throw new CustomUserMessageAuthenticationException('No API token provided');
-        // }
-
-        // implement your own logic to get the user identifier from `$apiToken`
-        // e.g. by looking up a user in the database using its API key
-        // $userIdentifier = /** ... */;
-
-        // return new SelfValidatingPassport(new UserBadge($userIdentifier));
+        $email = $request->request->get('_username', '');
+        $password = $request->request->get('_password', '');
+        
+        // Stocke l'email dans la session pour pré-remplir le formulaire en cas d'erreur
+        // $request->getSession()->set(Security::LAST_USERNAME, $email);
+    
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token'))
+            ]
+        );
     }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    
+    /**
+     * Redirige l'utilisateur après une authentification réussie en fonction de son rôle.
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?RedirectResponse
     {
         $user = $token->getUser();
         $roles = $user->getRoles();
@@ -60,20 +76,27 @@ class AppCustomAuthenticator extends AbstractAuthenticator
         return new RedirectResponse($targetUrl);
     }
     
-
+    /**
+     * Retourne l'URL de la page de connexion.
+     */
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate('app_login');
+    }
+    
+    /**
+     * Gère l'échec de l'authentification.
+     */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        $data = [
-            // you may want to customize or obfuscate the message first
-            'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
-
-            // or to translate this message
-            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
-        ];
-
-        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+        $request->getSession()->getFlashBag()->add('error', 'Authentification échouée : ' . $exception->getMessage());
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
     }
+}
 
+
+
+    
     // public function start(Request $request, ?AuthenticationException $authException = null): Response
     // {
     //     /*
@@ -84,4 +107,3 @@ class AppCustomAuthenticator extends AbstractAuthenticator
     //      * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
     //      */
     // }
-}
