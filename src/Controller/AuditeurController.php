4bcd5e2a02bad0bf5e37f955136c62fa8c48765f
Estherlvn <estherlvn@gmail.com>
2025-02-24 10:06:08 +0100

    lister les morceaux dans auditeur/home

--- src/Controller/AuditeurController.php
+++ src/Controller/AuditeurController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TrackRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class AuditeurController extends AbstractController
 {
     #[Route('/auditeur/home', name: 'auditeur_home')]
-    public function index(): Response
+    public function index(TrackRepository $trackRepository): Response
     {
+        // Récupérer tous les morceaux de la BDD
+        $tracks = $trackRepository->findAll();
+
         return $this->render('auditeur/home.html.twig', [
-            'controller_name' => 'AuditeurController',
+            'tracks' => $tracks,
         ]);
     }
 }
