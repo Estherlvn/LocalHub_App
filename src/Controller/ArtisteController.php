7e4737784d445bbe1ee727f94207b00c1bc2781e
Estherlvn <estherlvn@gmail.com>
2025-02-26 14:31:24 +0100

    un artiste peut supprimer un de ses tracks dans sa tracklist

--- src/Controller/ArtisteController.php
+++ src/Controller/ArtisteController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 
+use App\Entity\Track;
 use App\Repository\TrackRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -30,5 +33,28 @@ class ArtisteController extends AbstractController
                 ]);
             }
 
+    
+// Méthode pour supprimer un Track parmi la liste des pistes d'un artiste connecté
+    
+    #[Route('/track/{id}/delete', name: 'remove_track', methods: ['POST', 'GET'])]
+    #[IsGranted('ROLE_ARTISTE')]
+        public function removeTrack(Track $track, EntityManagerInterface $entityManager): RedirectResponse
+        {
+            $user = $this->getUser(); // Récupérer l’artiste connecté
+
+            // Vérifier que le morceau appartient bien à l'artiste connecté
+            if ($track->getUser() !== $user) {
+                throw $this->createAccessDeniedException("Vous ne pouvez supprimer que vos propres morceaux.");
+            }
+
+            // Supprimer le morceau de la base de données
+            $entityManager->remove($track);
+            $entityManager->flush();
+
+            // Message flash pour confirmer la suppression
+            $this->addFlash('success', 'Morceau supprimé avec succès.');
+
+            return $this->redirectToRoute('artiste_home'); // Redirection après suppression
+        }
 
 }
