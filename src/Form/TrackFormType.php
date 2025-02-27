6cbbf1cc1747bc49842b00584e233a0b7b461736
Estherlvn <estherlvn@gmail.com>
2025-02-27 15:31:46 +0100

    css form

--- src/Form/TrackFormType.php
+++ src/Form/TrackFormType.php
@@ -11,15 +11,37 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class TrackFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('trackName')
-            ->add('duration')
-            ->add('audioFile', FileType::class)
+        ->add('trackName', TextType::class, [
+            'label' => 'Titre du morceau',
+            'attr' => ['placeholder' => 'Entrez le titre du morceau'],
+        ])
+        ->add('duration', IntegerType::class, [
+            'label' => 'Durée (en secondes)',
+            'attr' => ['placeholder' => 'Ex: 180 pour 3 minutes'],
+        ])
+         ->add('genres', EntityType::class, [
+                'class' => Genre::class,
+                'choice_label' => 'genreName',
+                'multiple' => true,
+                'expanded' => true,
+                'attr' => ['class' => 'checkbox-group']
+        ])
+        ->add('audioFile', FileType::class, [
+            'label' => 'Fichier audio',
+            'required' => false, // Rendre facultatif si besoin
+            'attr' => ['accept' => 'audio/*'], // Autorise uniquement les fichiers audio
+
+        ]);
+    }
+    
             // ->add('uploadDate', null, [
             //     'widget' => 'single_text',
             // ])
@@ -27,13 +49,8 @@ class TrackFormType extends AbstractType
             //     'class' => User::class,
             //     'choice_label' => 'id',
             // ])
-            // ->add('genres', EntityType::class, [
-            //     'class' => Genre::class,
-            //     'choice_label' => 'id',
-            //     'multiple' => true,
-            // ])
-        ;
-    }
+           
+    
 
     public function configureOptions(OptionsResolver $resolver): void
     {
