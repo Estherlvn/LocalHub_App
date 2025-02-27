8897136aca9bb7d64fe459cfe02e1be4b4b7a7e9
Estherlvn <estherlvn@gmail.com>
2025-02-27 17:58:25 +0100

    lire un fichier audio uploadé (test) dans = track/show

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -14,43 +14,57 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrackController extends AbstractController
 {
-    // Afficher le formulaire d'ajout de morceau
+
+
+    // Lecteur audio pour lire une piste (test)
+    #[Route('/track/show/{id}', name: 'track_show')]
+        public function show(Track $track): Response
+        {
+            return $this->render('track/show.html.twig', [
+                'track' => $track,
+            ]);
+        }
+        
+
+
+
+    // AJOUTER un morceau / UPLOAD TRACK (dans la session d'un membre artiste connecté)
     #[Route('/track/add', name: 'add_track')]
     #[IsGranted('ROLE_ARTISTE')] // Seuls les artistes peuvent ajouter un morceau
-    public function addTrack(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
-    {
-        $user = $this->getUser(); // Récupérer l'artiste connecté
+        public function addTrack(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
+        {
+            $user = $this->getUser(); // Récupérer l'artiste connecté
 
-        $track = new Track();
-        $track->setUser($user); // Associer l’artiste au morceau
+            $track = new Track();
+            $track->setUser($user); // Associer l’artiste au morceau
 
-        $form = $this->createForm(TrackFormType::class, $track);
-        $form->handleRequest($request);
+            $form = $this->createForm(TrackFormType::class, $track);
+            $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var UploadedFile $audioFile */
-            $audioFile = $form->get('audioFile')->getData();
+            if ($form->isSubmitted() && $form->isValid()) {
+                /** @var UploadedFile $audioFile */
+                $audioFile = $form->get('audioFile')->getData();
 
-            // dump($audioFile); // Vérifier si le fichier est bien récupéré
-            // die();
+                // dump($audioFile); // Vérifier si le fichier est bien récupéré
+                // die();
 
-            if ($audioFile) {
-                // Utiliser le service FileUploader pour gérer l'upload
-                $fileName = $fileUploader->upload($audioFile);
-                $track->setAudioFile($fileName);
+                if ($audioFile) {
+                    // Utiliser le service FileUploader pour gérer l'upload
+                    $fileName = $fileUploader->upload($audioFile);
+                    $track->setAudioFile($fileName);
+                }
+
+                $entityManager->persist($track);
+                $entityManager->flush();
+
+                // Message flash de confirmation
+                $this->addFlash('success', 'Morceau ajouté avec succès !');
+
+                return $this->redirectToRoute('artiste_home'); // Redirection vers le formulaire
             }
 
-            $entityManager->persist($track);
-            $entityManager->flush();
-
-            // Message flash de confirmation
-            $this->addFlash('success', 'Morceau ajouté avec succès !');
-
-            return $this->redirectToRoute('add_track'); // Redirection vers le formulaire
+            return $this->render('track/add.html.twig', [
+                'form' => $form->createView(),
+            ]);
         }
-
-        return $this->render('track/form.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
 }
