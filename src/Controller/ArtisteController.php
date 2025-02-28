1248c936765f88eefc77677bf0a2089954175388
Estherlvn <estherlvn@gmail.com>
2025-02-28 16:49:14 +0100

    afficher et modifier son profil (artiste)

--- src/Controller/ArtisteController.php
+++ src/Controller/ArtisteController.php
@@ -23,36 +23,40 @@ class ArtisteController extends AbstractController
         }
 
 
-    // Permettre à un membre Artiste d'uploader une Image (photo de profil)
+    // Permettre à un membre Artiste de modifier son profil (dont upload de sa photo)
     #[Route('/artiste/profile', name: 'artiste_profile')]
     #[IsGranted('ROLE_ARTISTE')]
-        public function profile(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
-        {
-            /** @var User $user */
-            $user = $this->getUser();
-            $form = $this->createForm(ArtisteProfileType::class, $user);
-            $form->handleRequest($request);
-        
-            if ($form->isSubmitted() && $form->isValid()) {
-                $profilePictureFile = $form->get('profilePicture')->getData();
-        
-                if ($profilePictureFile) {
-                    // Utilisez le service FileUploader pour gérer l'upload
-                    $fileName = $fileUploader->upload($profilePictureFile);
-                    $user->setProfilePicture($fileName);
-                }
-        
-                $entityManager->persist($user);
-                $entityManager->flush();
-        
-                $this->addFlash('success', 'Votre profil a été mis à jour avec succès !');
-        
-                return $this->redirectToRoute('artiste_profile');
+    public function profile(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $form = $this->createForm(ArtisteProfileType::class, $user);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $profilePictureFile = $form->get('profilePicture')->getData();
+    
+            // dump($profilePicture); // Vérifier si le fichier est bien récupéré
+            // die();
+
+
+            if ($profilePictureFile) {
+                $fileName = $fileUploader->upload($profilePictureFile);
+                $user->setProfilePicture($fileName);
             }
-        
-            return $this->render('artist/profile.html.twig', [
-                'form' => $form->createView(),
-            ]);
+    
+            $entityManager->persist($user);
+            $entityManager->flush();
+    
+            $this->addFlash('success', 'Votre profil a été mis à jour avec succès !');
+    
+            return $this->redirectToRoute('artiste_profile');
         }
+    
+        return $this->render('artiste/home.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
     
 }
